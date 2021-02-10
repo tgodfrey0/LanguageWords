cfe7ac63a24f9b527100e757c3549a9ab69278ab
tg <toby.godfrey2003@gmail.com>
2021-02-10 13:01:34 +0000

    Opens files

--- Main.py
+++ Main.py
@@ -13,4 +13,28 @@ def selectTargetLanguage():
         print("\nPlease enter a valid option")
         selectTargetLanguage()
 
-target_language = selectTargetLanguage()
+def getWords(language):
+    """
+    subject = noun
+    adverb = adverb
+    adjective = adjective
+    direct_object = noun
+    indirect_object = noun
+    location = location
+    """
+    files = [open(str("./LanguagePacks/nouns/" + language), "r"),
+    open(str("./LanguagePacks/adverbs/" + language), "r"),
+    open(str("./LanguagePacks/adjectives/" + language), "r"),
+    open(str("./LanguagePacks/nouns/" + language), "r"),
+    open(str("./LanguagePacks/nouns/" + language), "r"),
+    open(str("./LanguagePacks/locations/" + language), "r")]
+    words = []
+    for i in range(7):
+        lines = files[i].readlines()
+        words.append(lines[random.randInt(0, lines.len()-1)])
+    # subject, adverb, adjective, direct_object, indirect_object, location
+    return words
+
+words = getWords(selectTargetLanguage())
+print(words)
+# subject, adverb, verb, adjective, direct object, indirect object, location
